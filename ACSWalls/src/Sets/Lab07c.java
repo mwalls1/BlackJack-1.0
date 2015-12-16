902339eaafd32ed708422fa562e79947d4d3f502
736620 <736620@10.9.33.59>
2015-12-16 09:15:26 -0600

    tried to do math sets

--- ACSWalls/src/Sets/Lab07c.java
+++ ACSWalls/src/Sets/Lab07c.java
@@ -6,6 +6,7 @@ package Sets;
 //Lab  -
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -14,6 +15,28 @@ public class Lab07c
 {
 	public static void main(String args[]) throws IOException
 	{
-		//add test cases
+		boolean a = true;
+		String one = "";
+		String two = "";
+		MathSet mySet;
+		try {
+			Scanner scan = new Scanner(new File("data/lab07c.dat"));
+			while(scan.hasNext())
+			{
+				if(a)
+				{
+					one+=scan.nextLine();
+				}
+				else
+					two+=scan.nextLine();
+				a=!a;
+				mySet = new MathSet(one,two);
+				System.out.println(mySet);
+				
+			}
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- ACSWalls/src/Sets/MathSet.java
+++ ACSWalls/src/Sets/MathSet.java
@@ -8,7 +8,12 @@ package Sets;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.Arrays;
+import java.util.Scanner;
+
 import static java.lang.System.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class MathSet
 {
@@ -17,39 +22,67 @@ public class MathSet
 
 	public MathSet()
 	{
+		one = new TreeSet<Integer>();
+		two = new TreeSet<Integer>();
 	}
 
 	public MathSet(String o, String t)
 	{
+		Scanner scan  = new Scanner(o);
+		while (scan.hasNextInt()) {
+			one.add(scan.nextInt());
+		}
+		Scanner sca  = new Scanner(t);
+		while (sca.hasNextInt()) {
+			two.add(sca.nextInt());
+		}
+
 	}
 
 	public Set<Integer> union()
 	{
-		return null;
+		Set<Integer> oo = new TreeSet<Integer>();
+		oo.addAll(one);
+		oo.addAll(two);
+		return oo;
+		
 	}
 
 	public Set<Integer> intersection()
 	{
-		return null;
+		Set<Integer> oo = new TreeSet<Integer>(one);
+		oo.retainAll(two);
+		return oo;
 	}
 
 	public Set<Integer> differenceAMinusB()
 	{
-		return null;
+		Set<Integer> oo = new TreeSet<Integer>(one);
+		oo.removeAll(two);
+		return oo;
 	}
 
 	public Set<Integer> differenceBMinusA()
 	{
-		return null;
+		Set<Integer> oo = new TreeSet<Integer>(two);
+		oo.removeAll(one);
+		return oo;
 	}
 	
 	public Set<Integer> symmetricDifference()
 	{		
-		return null;
+		Set<Integer> intersection = new TreeSet<Integer>(one);
+		intersection.retainAll(two);
+
+		Set<Integer> difference = new TreeSet<Integer>();
+		difference.addAll(one);
+		difference.addAll(two);
+		difference.removeAll(intersection);
+		return difference;
 	}	
 	
 	public String toString()
 	{
-		return "Set one " + one + "\n" +	"Set two " + two +  "\n";
+		return "Set one " + one + "\n" +	"Set two " + two +  "\n"+"union - "+union()+"\n"+"intersection - "+intersection()+"\n"+"Difference A-B - "+differenceAMinusB()+"\n"+"Difference B-A - "+differenceBMinusA()+"\n"+"Symmetric Difference - "+symmetricDifference();
 	}
 }
