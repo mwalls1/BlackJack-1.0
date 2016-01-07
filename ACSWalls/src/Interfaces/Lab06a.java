333585e8261a7b7334ba4602364f78d91ec2dd97
736620 <736620@10.9.33.96>
2016-01-07 09:13:56 -0600

    Started comparable

---
+++ ACSWalls/src/Interfaces/Lab06a.java
@@ -0,0 +1,29 @@
+package Interfaces;
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class Lab06a
+{
+	public static void main( String args[] ) throws IOException
+	{
+		//add test cases
+		ArrayList<Word> myList=new ArrayList<Word>();
+		Scanner scan=new Scanner(new File("data/lab06a.dat"));
+		while(scan.hasNE)
+		
+		
+		Word myWord = new Word();
+		System.out.println(myWord);
+	}
+}
---
+++ ACSWalls/src/Interfaces/Word.java
@@ -0,0 +1,36 @@
+package Interfaces;
+
+
+import static java.lang.System.*;
+
+import java.util.ArrayList;
+
+public class Word implements Comparable<Word> {
+	// add an instance variable and a constructor
+	// private ArrayList<String> words = new ArrayList<String>();
+	private String word = "";
+
+	public Word(String w) {
+		word = w;
+	}
+
+	// add a compareTo
+	public int compareTo(Word other) {
+		if (this.word.length() > other.word.length()) {
+			return 1;
+		} else if (this.word.length() < other.word.length()) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	public void setString(String a) {
+		word = a;
+	}
+
+	// add a toString
+	public String toString() {
+		return word;
+	}
+}
