c995d92c3f88b5d3c623dc30507246b834fca749
mwalls1 <walls594@gmail.com>
2017-05-23 17:59:55 -0500

    This is getting annoying
    Made aces work as a hybrid 11 and 1. The amount of logic that whent into it was too much. Adding this feature made a lot of other bugs that im still ironing out. Work well though

--- mason_code/application.windows32/source/mason_code.java
+++ mason_code/application.windows32/source/mason_code.java
@@ -17,12 +17,17 @@ public class mason_code extends PApplet {
 int dealer;
 int player;
 int faceDown;
+int rando;
+boolean aceIs11 = false;
+boolean dealerAceIs11 = false;
 boolean cosmic = false;
+int winnings;
 String filePath = dataPath("ace.png");
 Button hit;
 Button stay;
 Button replay;
 Button bet5;
+String hh = "+";
 Button bet10;
 Button bet15;
 int money = 100;
@@ -45,17 +50,17 @@ int b= floor(random(255));
 public void setup()
 {
   
-  hit = new Button(50,50, "Hit");
-  stay = new Button(300,50, "Stay");
-  replay = new Button(700,50,"Replay");
-  bet5 = new Button(50,200,"Bet $5");
-  bet10 = new Button(150,200,"Bet $10");
-  bet15 = new Button(250,200,"Bet $15");
+  hit = new Button(50, 50, "Hit");
+  stay = new Button(300, 50, "Stay");
+  replay = new Button(700, 50, "Replay");
+  bet5 = new Button(50, 200, "Bet $5");
+  bet10 = new Button(150, 200, "Bet $10");
+  bet15 = new Button(250, 200, "Bet $15");
 }
 public void draw()
 {
   textSize(15);
-  background(r,g,b);
+  background(r, g, b);
   paintPlayer();
   paintDealer();
   hit.draw();
@@ -64,39 +69,41 @@ public void draw()
   bet5.draw();
   bet10.draw();
   bet15.draw();
-  text("Player: "+player, 10,150);
-  text("Dealer: "+dealer, 100,150);
-  text("Cash: $"+money,200,150);
-  text("Bet: $"+bet,350,150);
-  if(madeBet == false)
+  text("Player: "+player, 10, 150);
+  text("Dealer: "+dealer, 100, 150);
+  text("Cash: $"+money, 200, 150);
+  text("Bet: $"+bet, 350, 150);
+  if (madeBet == false)
   {
-  textSize(50);
-  text("Please make a bet",300,500);
-  textSize(10);
+    textSize(50);
+    text("Please make a bet", 300, 500);
+    textSize(10);
   }
-  if(playerWin==true)
-  text("Player Wins!",500,150);
-  else if(dealerWin==true)
-  text("Dealer Wins!",500,150);
-  else if(noWIn==true)
-  text("Its a draw!",500,150);
-
+  if (playerWin==true)
+  {
+    text("Player Wins!", 500, 150);
+    text(hh+winnings,245,170);
+  }
+  else if (dealerWin==true)
+    text("Dealer Wins!", 500, 150);
+  else if (noWIn==true)
+  {
+    text(hh+winnings,245,170);
+    text("Its a draw!", 500, 150);
+  }
 }
 public void mousePressed()
 {
   int draw;
-  if(madeBet == false&&bet5.over()&&money>=5)
+  if (madeBet == false&&bet5.over()&&money>=5)
   {
-   // playSound = minim.loadFile("chipLay3.wav");
-    //playSound.play();
     newGame();
     bet+=5;
     money-=5;
     madeBet = true;
     isTurn = true;
     redraw();
-  }
-  else if(madeBet == false&&bet10.over()&&money>=10)
+  } else if (madeBet == false&&bet10.over()&&money>=10)
   {
     newGame();
     bet+=10;
@@ -104,8 +111,7 @@ public void mousePressed()
     madeBet = true;
     isTurn = true;
     redraw();
-  }
-  else if(madeBet == false&&bet15.over()&&money>=15)
+  } else if (madeBet == false&&bet15.over()&&money>=15)
   {
     newGame();
     bet+=15;
@@ -114,204 +120,278 @@ public void mousePressed()
     isTurn = true;
     redraw();
   }
-  if(hit.over()&&isTurn == true&&bust==false&&madeBet == true)
+  if (hit.over()&&isTurn == true&&bust==false&&madeBet == true)
   {
     draw = floor(random(cards.size()));
-    player+=cards.get(draw).value;
+    if(player == 10&&cards.get(draw).value==1)
+    {
+      player = 21;
+    }
+    else if(aceIs11==true&&player+cards.get(draw).value>21)
+    {
+      player+=cards.get(draw).value-10;
+      aceIs11 = false;
+    }
+    else 
+      player+=cards.get(draw).value;
     playersHand.add(cards.get(draw));
     cards.remove(draw);
     print("\nPlayers hand: "+player);
-    if(player == 21)
+    if (player == 21)
     {
       isTurn = false;
       play();
-    }
-    else if(player>21)
+    } 
+    else if (player>21)
     {
       bust = true;
       isTurn = false;
       play();
     }
+  } else if (stay.over()&&isTurn==true)
+  {
+    print("\nYou stay!");
+    isTurn = false;
+    play();
   }
-  else if(stay.over()&&isTurn==true)
-      {
-        print("\nYou stay!");
-        isTurn = false;
-        play();
-   }
-   if(replay.over()&&gameOver==true)
-   {
-     clear();
-   }
+  if (replay.over()&&gameOver==true)
+  {
+    clear();
+  }
 }
 public void play()
 {
-  if(player>21)
-  {
-    dealerWin = true;
-    gameOver = true;
-  }
-  if(player==21)
+  if (player==21)
   {
     playerWin = true;
-    money+=bet*3;
+    winnings = bet*3;
+    money+=winnings;
     bet = 0;
     gameOver = true;
   }
-  int draw;
-  if(isTurn == false&&bust == false&& gameOver == false)
+  if(dealer+faceDown==21)
+  { 
+    dealersHand.add(faceDownCard);
+    dealer = 21;
+    gameOver = true;
+    bet = 0;
+    dealerWin = true;
+  }
+  if (bust)
   {
+    dealerWin = true;
+    bet = 0;
+    gameOver = true;
+  }
+  
+  int draw;
+  if (isTurn == false&&bust == false&& gameOver == false)
+  {
+    if(faceDown==1)
+      faceDown=11;
     dealer+=faceDown;
     dealersHand.add(faceDownCard);
     print("\nDealers hand: "+dealer);
-    while(dealer<17)
+    while (dealer<17)
     {
       draw = floor(random(cards.size()));
-      dealer+=cards.get(draw).value;
+      if(dealerAceIs11 == true&&dealer+cards.get(draw).value>21)
+      {
+        dealer+=cards.get(draw).value-10;
+        dealerAceIs11 = false;
+      }
+      else
+        dealer+=cards.get(draw).value;
       dealersHand.add(cards.get(draw));
       print("\nDealer draws: "+cards.get(draw).value);
       cards.remove(draw);
       print("\nDealers hand: "+dealer);
       redraw();
     }
-    if(dealer>player&&dealer<=21)
+    if(dealer==21)
     {
-    print("\nDealer Wins!");
-    dealerWin = true;
-    gameOver = true;
+      dealerWin = true;
+      bet = 0;
+      gameOver = true;
     }
-    else if(player==dealer)
+    else if (dealer>player&&dealer<21)
     {
-    print("\nDraw!");
-    gameOver = true;
-    money+=bet;
-    noWIn = true;
-    }
-    else
+      print("\nDealer Wins!");
+      dealerWin = true;
+      gameOver = true;
+      bet = 0;
+    } else if (player==dealer&&player<21)
     {
-    print("\nPlayer Wins!");
-    gameOver = true;
-    money+=bet*2;
-    playerWin = true;
+      print("\nDraw!");
+      gameOver = true;
+      winnings = bet;
+      money+=winnings;
+      noWIn = true;
+      bet = 0;
+    } else
+    {
+      print("\nPlayer Wins!");
+      gameOver = true;
+      winnings = bet*2;
+      money+=winnings;
+      playerWin = true;
+      bet = 0;
     }
-  }
-  if(bust == true)
-  {
-  dealerWin = true;
-  print("\nDealer Wins!");
-  gameOver = true;
   }
 }
 public void paintPlayer()
 {
-  for(int i =0; i<playersHand.size(); i++)
+  for (int i =0; i<playersHand.size(); i++)
   {
-    image(playersHand.get(i).image, 100+i*110,400);
+    image(playersHand.get(i).image, 100+i*110, 400);
   }
 }
 public void paintDealer()
 {
-  for(int i =0; i<dealersHand.size(); i++)
+  for (int i =0; i<dealersHand.size(); i++)
   {
-    image(dealersHand.get(i).image, 900+i*110,400);
+    image(dealersHand.get(i).image, 900+i*110, 400);
   }
 }
 public void newGame()
 {
 
-  cards.add(new card(1,loadImage("ace.png")));
-  cards.add(new card(1,loadImage("ace.png")));
-  cards.add(new card(1,loadImage("ace.png")));
-  cards.add(new card(1,loadImage("ace.png")));
-  cards.add(new card(2,loadImage("2.png")));
-  cards.add(new card(2,loadImage("2.png")));
-  cards.add(new card(2,loadImage("2.png")));
-  cards.add(new card(2,loadImage("2.png")));
-  cards.add(new card(3,loadImage("3.png")));
-  cards.add(new card(3,loadImage("3.png")));
-  cards.add(new card(3,loadImage("3.png")));
-  cards.add(new card(3,loadImage("3.png")));
-  cards.add(new card(4,loadImage("4.png")));
-  cards.add(new card(4,loadImage("4.png")));
-  cards.add(new card(4,loadImage("4.png")));
-  cards.add(new card(4,loadImage("4.png")));
-  cards.add(new card(5,loadImage("5.png")));
-  cards.add(new card(5,loadImage("5.png")));
-  cards.add(new card(5,loadImage("5.png")));
-  cards.add(new card(5,loadImage("5.png")));
-  cards.add(new card(6,loadImage("6.png")));
-  cards.add(new card(6,loadImage("6.png")));
-  cards.add(new card(6,loadImage("6.png")));
-  cards.add(new card(6,loadImage("6.png")));
-  cards.add(new card(7,loadImage("7.png")));
-  cards.add(new card(7,loadImage("7.png")));
-  cards.add(new card(7,loadImage("7.png")));
-  cards.add(new card(7,loadImage("7.png")));
-  cards.add(new card(8,loadImage("8.png")));
-  cards.add(new card(8,loadImage("8.png")));
-  cards.add(new card(8,loadImage("8.png")));
-  cards.add(new card(8,loadImage("8.png")));
-  cards.add(new card(9,loadImage("9.png")));
-  cards.add(new card(9,loadImage("9.png")));
-  cards.add(new card(9,loadImage("9.png")));
-  cards.add(new card(9,loadImage("9.png")));
-  cards.add(new card(10,loadImage("10.png")));
-  cards.add(new card(10,loadImage("10.png")));
-  cards.add(new card(10,loadImage("10.png")));
-  cards.add(new card(10,loadImage("10.png")));
-  cards.add(new card(10,loadImage("jack.png")));
-  cards.add(new card(10,loadImage("jack.png")));
-  cards.add(new card(10,loadImage("jack.png")));
-  cards.add(new card(10,loadImage("jack.png")));
-  cards.add(new card(10,loadImage("queen.png")));
-  cards.add(new card(10,loadImage("queen.png")));
-  cards.add(new card(10,loadImage("queen.png")));
-  cards.add(new card(10,loadImage("queen.png")));
-  cards.add(new card(10,loadImage("king.jpg")));
-  cards.add(new card(10,loadImage("king.jpg")));
-  cards.add(new card(10,loadImage("king.jpg")));
-  cards.add(new card(10,loadImage("king.jpg")));
+  cards.add(new card(1, loadImage("ace.png")));
+  cards.add(new card(1, loadImage("ace.png")));
+  cards.add(new card(1, loadImage("ace.png")));
+  cards.add(new card(1, loadImage("ace.png")));
+  cards.add(new card(2, loadImage("2.png")));
+  cards.add(new card(2, loadImage("2.png")));
+  cards.add(new card(2, loadImage("2.png")));
+  cards.add(new card(2, loadImage("2.png")));
+  cards.add(new card(3, loadImage("3.png")));
+  cards.add(new card(3, loadImage("3.png")));
+  cards.add(new card(3, loadImage("3.png")));
+  cards.add(new card(3, loadImage("3.png")));
+  cards.add(new card(4, loadImage("4.png")));
+  cards.add(new card(4, loadImage("4.png")));
+  cards.add(new card(4, loadImage("4.png")));
+  cards.add(new card(4, loadImage("4.png")));
+  cards.add(new card(5, loadImage("5.png")));
+  cards.add(new card(5, loadImage("5.png")));
+  cards.add(new card(5, loadImage("5.png")));
+  cards.add(new card(5, loadImage("5.png")));
+  cards.add(new card(6, loadImage("6.png")));
+  cards.add(new card(6, loadImage("6.png")));
+  cards.add(new card(6, loadImage("6.png")));
+  cards.add(new card(6, loadImage("6.png")));
+  cards.add(new card(7, loadImage("7.png")));
+  cards.add(new card(7, loadImage("7.png")));
+  cards.add(new card(7, loadImage("7.png")));
+  cards.add(new card(7, loadImage("7.png")));
+  cards.add(new card(8, loadImage("8.png")));
+  cards.add(new card(8, loadImage("8.png")));
+  cards.add(new card(8, loadImage("8.png")));
+  cards.add(new card(8, loadImage("8.png")));
+  cards.add(new card(9, loadImage("9.png")));
+  cards.add(new card(9, loadImage("9.png")));
+  cards.add(new card(9, loadImage("9.png")));
+  cards.add(new card(9, loadImage("9.png")));
+  cards.add(new card(10, loadImage("10.png")));
+  cards.add(new card(10, loadImage("10.png")));
+  cards.add(new card(10, loadImage("10.png")));
+  cards.add(new card(10, loadImage("10.png")));
+  cards.add(new card(10, loadImage("jack.png")));
+  cards.add(new card(10, loadImage("jack.png")));
+  cards.add(new card(10, loadImage("jack.png")));
+  cards.add(new card(10, loadImage("jack.png")));
+  cards.add(new card(10, loadImage("queen.png")));
+  cards.add(new card(10, loadImage("queen.png")));
+  cards.add(new card(10, loadImage("queen.png")));
+  cards.add(new card(10, loadImage("queen.png")));
+  cards.add(new card(10, loadImage("king.jpg")));
+  cards.add(new card(10, loadImage("king.jpg")));
+  cards.add(new card(10, loadImage("king.jpg")));
+  cards.add(new card(10, loadImage("king.jpg")));
   int draw;
   draw = floor(random(cards.size()));
-  player+=cards.get(draw).value;
-  playersHand.add(cards.get(draw));
+  if (cards.get(draw).value==1)
+  {
+    player+=11;
+    aceIs11=true;
+    playersHand.add(cards.get(draw));
+    cards.remove(draw);
+  }
+  else
+  {
+    player+=cards.get(draw).value;
+    playersHand.add(cards.get(draw));
+    cards.remove(draw);
+  }
+  draw = floor(random(cards.size()));
+  faceDown = cards.get(draw).value;
+  faceDownCard = cards.get(draw);
   cards.remove(draw);
   draw = floor(random(cards.size()));
-  faceDown=cards.get(draw).value;
-      faceDownCard = cards.get(draw);
-      cards.remove(draw);
-      draw = floor(random(cards.size()));
-      player+=cards.get(draw).value;
+  if (cards.get(draw).value==1)
+  {
+    if(player<=10)
+    {
+      player+=11;
+      aceIs11 =true;
       playersHand.add(cards.get(draw));
       cards.remove(draw);
-      draw = floor(random(cards.size()));
-      dealer+=cards.get(draw).value;
-      dealersHand.add(cards.get(draw));
-      cards.remove(draw);
-      if(player>21)
-      bust = true;
-      print("\nPlayers hand: "+player);
-      gameOn = true;
+    }
+    else
+    rando = 0;
+  }
+  else
+  {
+    player+=cards.get(draw).value;
+    playersHand.add(cards.get(draw));
+    cards.remove(draw);
+  }
+  draw = floor(random(cards.size()));
+  if(cards.get(draw).value==1)
+  {
+    dealer+=11;
+  dealerAceIs11 = true;
+  }
+  else
+    dealer+=cards.get(draw).value;
+  dealersHand.add(cards.get(draw));
+  cards.remove(draw);
+  if (player == 21)
+    {
+      isTurn = false;
+      gameOver = true;
+      play();
+    } 
+  if (player>21)
+    bust = true;
+  print("\nPlayers hand: "+player);
+  gameOn = true;
+  
+    if(dealer+faceDown==21)
+    {
+      play();
+    }
 }
 public void clear()
 {
-  bet = 0;
   player = 0;
   dealer = 0;
-  while(cards.size()>0)
+  while (cards.size()>0)
     cards.remove(0);
-  while(dealersHand.size()>0)
+  while (dealersHand.size()>0)
     dealersHand.remove(0);
-   while(playersHand.size()>0)
-     playersHand.remove(0);
-     madeBet = false;
-     bust = false;
-     isTurn = false;
-     playerWin = false; 
-     dealerWin = false;
-     noWIn = false; 
-     gameOver = false; 
-     gameOn = false;
+  while (playersHand.size()>0)
+    playersHand.remove(0);
+  madeBet = false;
+  aceIs11 = false;
+  bust = false;
+  isTurn = false;
+  playerWin = false; 
+  dealerWin = false;
+  noWIn = false; 
+  gameOver = false; 
+  gameOn = false;
+  dealerAceIs11 = false;
 }
 class Button{
   int x,y;
